994c6710e6b1614a5135158995342619b3093751
Andrii Chychkan <mywork1728@gmail>
2025-06-01 22:54:43 +0300

    added search query

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <div class="weather"></div> -->
+</template>
+
+<script>
+export default {
+  data() {
+    this.weaterSearch()
+    return {
+
+      }
+  },
+    // computed: {
+
+    // }
+    methods: {
+    weaterSearch() {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=milan&appid=8ffe6449c5da40e64905d7a9f8d0ee6e&units=metric").then(response => response.json()).then(data => console.log(data))
+      },
+      // resetSearchQuery() {
+
+      // }
+    }
+  }
+</script>
